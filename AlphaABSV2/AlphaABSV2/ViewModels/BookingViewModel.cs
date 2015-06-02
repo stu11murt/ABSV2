49cf80e047c09a2c763a3480e3b7d6cfa4325450
Stuart Murton <stuart@deepbluedevelopment.co.uk>
2015-06-02 09:53:09 +0100

    More commits (initial)

---
+++ AlphaABSV2/AlphaABSV2/ViewModels/BookingViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AlphaABSV2.Models;
+using AlphaABSV2.ViewModels;
+using AlphaABSV2.DAL;
+using System.Web.Mvc;
+
+namespace AlphaABSV2.ViewModels
+{
+    public class BookingViewModel
+    {
+        private ABSContext db = new ABSContext();
+
+        public BookingForm booking { get; set; }
+        public List<EventRecord> EventData { get; set; }
+        public List<Event> DynamicEvents
+        {
+            get
+            {
+
+                return db.Events.ToList();
+
+            }
+            set
+            {
+
+
+            }
+        }
+    }
+}
